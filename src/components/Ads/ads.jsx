b16d53d2ae8bf7c6411c10ca38dfea5d5f9f4c2a
Francisco Mackinlay <franmackinlay@gmail.com>
2020-02-21 18:07:26 -0300

    fixed reload

--- src/components/Ads/ads.jsx
+++ src/components/Ads/ads.jsx
@@ -16,10 +16,12 @@ export default class Ads extends Component {
     this.state = {
       adsList: []
     }
-    this.getAdsList();
-
+    this.fetchAds();
   }
 
+  fetchAds = async () => {
+    this.getAdsList();
+  }
 
   getAdsList = async () => {
     const ads = await getAds();
@@ -30,18 +32,22 @@ export default class Ads extends Component {
   }
 
   render() {
+    this.fetchAds();
     const { adsList } = this.state;
 
     return (
       <div>
         <h1>Anuncios</h1>
+        <Link to="/createAd">
+          <button className="create-ad">Crear Anuncio</button>
+        </Link>
         <ul>
 
           {adsList.map(ad => {
             return (
               <Link to={`/detail/${ad._id}`}>
                 <li key={ad._id}>
-                  <img src={ad.photo} alt="Ad Image" />
+                  <img src={ad.photo} alt="AdImage" />
                   <p>{ad.name}</p>
                   <p>{ad.price}</p>
                   <p>{ad.type}</p>
