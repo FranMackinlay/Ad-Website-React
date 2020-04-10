a3b5985ec53d41f199d6c1530f35c8173b8cf4e1
Fran Mackinlay <franmackinlay@gmail.com>
2020-04-09 21:15:43 -0300

    editAd re-styling finished

--- src/components/EditAd/editAd.jsx
+++ src/components/EditAd/editAd.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import { Link } from "react-router-dom";
+import './editAd.css';
 
 const { getAdDetail, editAd } = api();
 
@@ -65,22 +66,39 @@ export default class EditAd extends Component {
     const { name, price, description, tags, type, photo } = this.state;
     return (
       <div>
-        <Link to="/anuncios">
-          <button>Atras</button>
+        <Link className="back-to-ads" to="/anuncios">
+          <button className="back-btn">Atras</button>
         </Link>
-        <h1>Editar anuncio de: {name}</h1>
-        <form onSubmit={this.onSubmit}>
-          <input onChange={this.handleInput} value={name} name="name" type="text" placeholder="Ad Name" />
-          <input onChange={this.handleInput} value={price} name="price" type="text" placeholder="Ad Price" />
-          <input onChange={this.handleInput} value={description} name="description" type="text" placeholder="Ad Description" />
-          <input onChange={this.handleInput} value={tags} name="tags" type="text" placeholder="Ad Tags" />
-          <input onChange={this.handleInput} value={type} name="type" type="text" placeholder="Ad Type" />
-          <input onChange={this.handleInput} value={photo} name="photo" type="text" placeholder="Ad Photo" />
-          <button type="submit">Editar Anuncio</button>
-        </form>
+        <h1>Ad to be edited: "{name}"</h1>
+        <form className="edit-ad-form" onSubmit={this.onSubmit}>
+          <div className="edit-name">
+            <label htmlFor="name">Name:</label>
+            <input id="name" onChange={this.handleInput} value={name} name="name" type="text" placeholder="Ad Name" />
+          </div>
+          <div className="edit-price">
+            <label htmlFor="price">Price:</label>
+            <input id="price" onChange={this.handleInput} value={price} name="price" type="text" placeholder="Ad Price" />
+          </div>
+          <div className="edit-description">
+            <label htmlFor="description">Description:</label>
+            <input id="description" onChange={this.handleInput} value={description} name="description" type="text" placeholder="Ad Description" />
+          </div>
+          <div className="edit-tags">
+            <label htmlFor="tags">Tags:</label>
+            <input onChange={this.handleInput} value={tags} name="tags" type="text" placeholder="Ad Tags" />
+          </div >
+          <div className="edit-type">
+            <label htmlFor="type">Type:</label>
+            <input onChange={this.handleInput} value={type} name="type" type="text" placeholder="Ad Type" />
+          </div>
+          <div className="edit-photo">
+            <label htmlFor="photo">Photo</label>
+            <input onChange={this.handleInput} value={photo} name="photo" type="text" placeholder="Ad Photo" />
+          </div>
+          <button className="save-btn" type="submit">Save Changes</button>
+        </form >
         <br />
-
-      </div>
+      </div >
 
     )
   }
