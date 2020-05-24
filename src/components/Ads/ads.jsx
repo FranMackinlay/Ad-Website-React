f661d77b8da63df750bdb90c6e425be16cbe41a4
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-24 19:34:02 -0300

    form refactor

--- src/components/Ads/ads.jsx
+++ src/components/Ads/ads.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import Header from '../Header/header';
-import filterParams from '../../const/filterParams';
+
 import Card from '../Card/card';
+import FilterForm from '../FilterForm/filterForm';
 import './ads.css';
 
 const { getAds, filterAd } = api();
@@ -19,8 +20,6 @@ export default class Ads extends Component {
 		super(props);
 		this.state = {
 			adsList: [],
-			filterInput: '',
-			filterSelect: filterParams[0].id,
 			filteredAdsList: null,
 		};
 	}
@@ -41,51 +40,30 @@ export default class Ads extends Component {
 		}
 	};
 
-	onType = event => {
-		const value = event.target.value;
-		if (value === 'buy') {
-			this.setState({
-				filterInput: false,
-			});
-		} else if (value === 'sell') {
-			this.setState({
-				filterInput: true,
-			});
-		} else {
-			this.setState({
-				filterInput: event.target.value,
-			});
-		}
-	};
-
 	filterAds = async (query, value) => {
 		const filteredAds = await filterAd(query, value);
 		return filteredAds;
 	};
 
-	onSelect = event => {
-		this.setState({
-			filterSelect: event.target.value,
-		});
-	};
-
-	onSubmit = async event => {
-		event.preventDefault();
+	onSubmit = async data => {
 		this.onResetFilter();
-		const adsWithFilter = await this.filterAds(`${this.state.filterSelect}`, `${this.state.filterInput}`);
+		const { filterSelect, filterInput } = data;
+		console.log(filterSelect, filterInput);
+		const adsWithFilter = await this.filterAds(`${filterSelect}`, `${filterInput}`);
 		if (adsWithFilter.error) {
 			alert('Please type a valid filter');
 		} else {
-			adsWithFilter.results.map(adFiltered => {
-				return filteredAdsArray.push(adFiltered);
-			});
+			adsWithFilter.results.forEach(adFiltered => filteredAdsArray.push(adFiltered));
 			this.setState({
 				filteredAdsList: filteredAdsArray,
 			});
 		}
+		console.log(data);
 	};
 
 	onResetFilter = event => {
+		console.log('reset filter');
+
 		if (event) {
 			event.preventDefault();
 		}
@@ -101,29 +79,14 @@ export default class Ads extends Component {
 	renderFilteredAdsList = filteredAdsList => filteredAdsList.map(ad => <Card key={ad._id} ad={ad} />);
 
 	render() {
+		console.log('render');
 		const { adsList, filteredAdsList } = this.state;
+		console.log(adsList, filteredAdsList);
 		if (!adsList) return <h1>Loading Ads...</h1>;
 		return (
 			<div className='content-container'>
 				<Header />
-				<form className='ads-form' onSubmit={this.onSubmit}>
-					<select className='select-form' onChange={this.onSelect}>
-						{filterParams.map(({ id, param }) => {
-							return (
-								<option key={id} value={id}>
-									{param}
-								</option>
-							);
-						})}
-					</select>
-					<input className='input-form' onChange={this.onType} type='text' />
-					<button className='search-form' type='submit'>
-						Search
-					</button>
-					<button className='reset-form' onClick={this.onResetFilter}>
-						Reset
-					</button>
-				</form>
+				<FilterForm onSubmit={this.onSubmit} onResetFilter={this.onResetFilter} />
 				<div className='ads-container'>
 					<ul>{filteredAdsList === null ? this.renderAdList(adsList) : this.renderFilteredAdsList(filteredAdsList)}</ul>
 				</div>
