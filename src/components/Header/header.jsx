e81a197025db067a33317457791b6a2b25caec45
Fran Mackinlay <franmackinlay@gmail.com>
2020-06-01 14:30:51 -0300

    primeReact menubar and filterForm

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,30 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { Menubar } from 'primereact/menubar';
+import FilterForm from '../FilterForm/filterForm';
 import './header.css';
 
-export default function Header() {
+export default function Header({ onSubmit, onResetFilter }) {
 	return (
-		<Fragment>
-			<Link to='/login' className='log-out-container'>
-				<button className='log-out'>Log Out</button>
-			</Link>
-			<div className='main-container'>
-				<div className='first-container share'>
-					<h1 className='title'>
-						<span id='one'>R</span>
-						<span>e</span>
-						<span>a</span>
-						<span>c</span>
-						<span>t</span>
-						<span>A</span>
-						<span>d</span>
-						<span>s</span>
-					</h1>
-				</div>
+		<Menubar>
+			<div className='logo-container'>
+				<h1 className='title'>ReactAds</h1>
 			</div>
-			<Link to='/createAd'>
-				<button className='create-ad'>Create Ad</button>
-			</Link>
-		</Fragment>
+			<div className='form-container'>
+				<FilterForm onSubmit={onSubmit} onResetFilter={onResetFilter} />
+			</div>
+			<div className='menu-container'>
+				<Link to='/createAd'>
+					<button className='create-ad'>Create Ad</button>
+				</Link>
+				<Link to='/login' className='log-out-container'>
+					<button className='log-out'>Log Out</button>
+				</Link>
+			</div>
+		</Menubar>
 	);
 }
