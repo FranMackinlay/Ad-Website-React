25b13d66107773586e431842ad2eee0bc342c778
Fran Mackinlay <franmackinlay@gmail.com>
2020-06-03 18:02:21 -0300

    header component created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+	return (
+		<div className='header-container'>
+			<span className='text1'>Welcome to ReactAds!</span>
+			<span className='text2'>What are you looking for today?</span>
+		</div>
+	);
+}
