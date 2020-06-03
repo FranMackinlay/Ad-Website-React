4815f3773d943d7360257342ba8337b10dadd0e0
Fran Mackinlay <franmackinlay@gmail.com>
2020-06-03 18:02:46 -0300

    refactor to display navbar and header

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { ProgressSpinner } from 'primereact/progressspinner';
-import Header from '../Navbar/Navbar';
+import Navbar from '../Navbar/Navbar';
+import Header from '../Header/Header';
 import './loading.css';
 
 export default function Loading(props) {
 	const { onSubmit, onResetFilter } = props;
 	return (
 		<div className='content-container'>
-			<Header onSubmit={onSubmit} onResetFilter={onResetFilter} />
+			<div className='header-navbar-container'>
+				<Navbar onSubmit={onSubmit} onResetFilter={onResetFilter} />
+				<Header />
+			</div>
 			<div className='ads-container'>
 				<ProgressSpinner />
 			</div>
